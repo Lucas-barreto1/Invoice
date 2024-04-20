83e097bd8f6d4e3e3b8420608c06c8dda97e54cb
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2024-04-20 10:32:27 -0300

    refactor: changing connection with sqlserver to postgres in context of efcore

--- Invoice.Collectors.Infra.Data.Context/InvoiceCollectorsContextConfiguration.cs
+++ Invoice.Collectors.Infra.Data.Context/InvoiceCollectorsContextConfiguration.cs
@@ -11,18 +11,13 @@ namespace Invoice.Collectors.Context
             IConfiguration configuration
         )
         {
-            var connectionString = configuration.GetConnectionString("HTCollectors");
+            var connectionString = configuration.GetConnectionString("DBConnection");   
 
             services.AddDbContext<InvoiceCollectorsContext>(builder =>
             {
-                builder.UseSqlServer(connectionString, options =>
+                builder.UseNpgsql(connectionString, options =>
                 {
-                    options.EnableRetryOnFailure(
-                            maxRetryCount: 2,
-                            maxRetryDelay: TimeSpan.FromSeconds(5),
-                            errorNumbersToAdd: null
-                        )
-                        .MigrationsAssembly("Invoice.Collectors.Infra.Data.Migrations");
+                    options.MigrationsAssembly("Invoice.Collectors.Infra.Data.Migrations");
 
                     // opt.MigrationsHistoryTable("EFMigrationsHistoryCurso"); // É possível alterar o nome da tabela onde são armazenados registros de execução do migrations (a tabela padrão do EF para histórico).
                     // IMPORTANTE: Isso deve ser feito antes do primeiro deploy, se não, a tabela anterior (nome padrão) continua.
